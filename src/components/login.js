b8a6c01f18145196dcc3646f6dfc04e03ec22080
jeanellach <jeanellach@gmail.com>
2022-01-27 09:40:37 -0500

    firts functions

---
+++ src/components/login.js
@@ -0,0 +1,24 @@
+// Creacion de formulario de inicio de Sesión de forma dinámica
+export const formLogin = () => {
+    const form = `
+          <div id="inicio" class="cajaInterna2">
+              <form id="formIngreso">
+                  <div class="seccionIngreso">
+                      <input type="text" id="correoIngreso" class="datosIngreso" placeholder="Correo electrónico" required>
+                      <i class="ph-envelope"></i>
+                  </div>
+                  <div class="seccionIngreso">
+                      <input type="password" id="claveIngreso" class="datosIngreso" placeholder="Contraseña" required>
+                      <i id="botonContraseña" class="ph-eye-closed"></i>
+                  </div>
+                  <button type="submit" id="botonIngresar" class="iniciarSesion">Ingresar</button>
+                  <p class="texto">O bien ingresa con</p>
+                  <div class="logosInicio">
+                      <img id="imgFacebook" src="imagenes/FacebookOriginal.png">
+                      <img id="imgGoogle" src="imagenes/GoogleOriginal.png">
+                  </div>
+                  <p class="texto">¿No tienes una cuenta? <a id="registrate" href="#/registro"> Regístrate</a></p> 
+              </form>
+          </div>`;
+    return form;
+  };
---
+++ src/view-controller/router.js
@@ -0,0 +1,5 @@
+import {login} from '../components/login.js'
+
+export const componentsViews = () => {
+    const main = document.getElementById('main')
+}
