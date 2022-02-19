f562408dc83aa20bd9005a9f2c0d700d1938d8c6
anakarina1928 <anakarina.nak@gmail.com>
2022-02-19 12:40:33 -0500

    funcion para ordenar post

--- src/firebase/feed.js
+++ src/firebase/feed.js
@@ -1,5 +1,5 @@
 import { db } from "./config.js";
-import { collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js"
+import { collection, addDoc, getDocs, onSnapshot, deleteDoc, doc,getDoc } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js"
 import { userDataLocally } from "../components/sessionStorage.js";
 //inserto un new post en firestorage
 export const insertData = async (post) => {
@@ -28,3 +28,4 @@ export const itemsProfie = (callback) => onSnapshot(collection(db, "usuario"), c
 //export const orderByPost = () => query(collection(db, "post_user"), orderBy('timestamp')); necesito saber donde insertarlo (recuerda que lo dejaste de importar)
 export const onDataDocument = (callback) => onSnapshot(collection(db, "post_user"), callback)
 export const deletePost = (id) => deleteDoc(doc(db, "post_user", id))
+export const getDocument = (id) => getDoc(doc(db,"post_user",id))
